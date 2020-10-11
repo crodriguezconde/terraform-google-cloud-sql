714099fd62e2a827b4062d96fae4d177e106e87f
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-11 22:03:51 +0200

    First commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "google_sql_database_instance" "mysql_instance" {
+	name             = var.name
+  database_version = var.database_version
+  region           = var.cloud_sql_region
+
+  settings {
+    tier              = var.tier
+    activation_policy = var.activation_policy
+    availability_type = var.availability_type
+    disk_autoresize   = var.disk_autoresize
+    disk_size         = var.disk_size
+    disk_type         = var.disk_type
+    backup_configuration {
+      binary_log_enabled = var.binary_log_enabled
+    }
+    ip_configuration {
+      ipv4_enabled    = var.ipv4_enabled
+      private_network = var.vpc_network_id
+      require_ssl     = var.require_ssl
+    }
+    maintenance_window {
+      day          = var.maintenance_prefered_day
+      hour         = var.maintenance_prefered_hour
+      update_track = var.update_track
+    }
+  }
+}
