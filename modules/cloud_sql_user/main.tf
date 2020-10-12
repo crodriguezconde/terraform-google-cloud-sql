475bbbda6e760882395a26209acbd62783185429
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-12 12:17:39 +0200

    Functionality to add new Cloud SQL users

---
+++ modules/cloud_sql_user/main.tf
@@ -0,0 +1,8 @@
+provider "google" { project = var.project_id}
+
+resource "google_sql_user" "new_csql_user" {
+	name = var.sql_user_name
+	instance = var.cloud_sql_instance_name
+	password = var.sql_user_password
+}
+
---
+++ modules/cloud_sql_user/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {}
+
+variable "sql_user_name" {
+	type = string
+	description = "(Required) The name of the user."
+}
+
+variable "cloud_sql_instance_name" {
+	type = string
+	description = "(Required) The name of the Cloud SQL instance the user will be created on."
+}
+
+variable "sql_user_password" {
+	type = string
+	description = "(Optional) The password for the user."
+}
