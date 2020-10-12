e5717f24de13cb9d4186b95966b99f8a5f7582e1
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-12 12:33:24 +0200

    Added cloud sql user module to the example

--- modules/examples/public_ip_mysql_instance/main.tf
+++ modules/examples/public_ip_mysql_instance/main.tf
@@ -19,4 +19,14 @@ module "public_mysql_instance" {
   cloud_sql_region = var.instance_region
 }
 
+# Second-generation instances include a default 'root'@'%' user with no password. 
+#This user will be deleted by Terraform on instance creation. 
+# Therefore, we will create a google_sql_user to define a custom user with a restricted host and strong password.
 
+module "cloud_sql_user" {
+	source = "git::https://github.com/crodriguezconde/cloud_sql.git//modules/cloud_sql_user"
+
+	sql_user_name = "terraform-user"
+	cloud_sql_instance_name = module.public_mysql_instance.name
+	sql_user_password = ""
+}
