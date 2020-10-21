e8d722dcd8d24168c568a6ee45edc1661d3f41b7
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-21 12:03:17 +0200

    Added output variables to the public IP address example

---
+++ modules/examples/public_ip_mysql_instance/outputs.tf
@@ -0,0 +1,15 @@
+output "self_link" {
+	value = module.public_mysql_instance.self_link
+}
+
+output "connection_name" {
+	value = module.public_mysql_instance.connection_name
+}
+
+output "public_ip_address" {
+	value = module.public_mysql_instance.public_ip_address
+}
+
+output "name" {
+	value = module.public_mysql_instance.name
+} 
