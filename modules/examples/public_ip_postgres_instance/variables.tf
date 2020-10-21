095cfe6445d98c158c2503b863b367f047d1775f
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-21 12:50:04 +0200

    Added variables declared in main.tf

--- modules/examples/public_ip_postgres_instance/variables.tf
+++ modules/examples/public_ip_postgres_instance/variables.tf
@@ -1,2 +1,11 @@
-variable "sql_user_password" {}
 variable "project_id" {}
+
+### Variables declaration for Cloud SQL instance
+variable "instance_name" {}
+variable "sql_region" {}
+variable "postgres_version" {}
+
+
+## Variables declaration for Cloud SQL user
+variable "sql_user_name" {}
+variable "sql_user_password" {}
