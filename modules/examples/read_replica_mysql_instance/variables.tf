3061cc50fc94afe9c139352fdc4b1b8b77cd6aba
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-21 13:08:26 +0200

    Added variable declaration for main.tf

--- modules/examples/read_replica_mysql_instance/variables.tf
+++ modules/examples/read_replica_mysql_instance/variables.tf
@@ -1,2 +1,17 @@
-variable "sql_user_password" {}
 variable "project_id" {}
+
+### Variables declaration for Cloud SQL instance (master)
+variable "mysql_version" {}
+variable "instance_name" {}
+variable "sql_region" {}
+
+### Variable declaration for Cloud SQL replica instance
+
+variable "number_of_replicas" {}
+
+
+
+### Variable declaration for Cloud SQL user
+
+variable "sql_user_name" {}
+variable "sql_user_password" {}
