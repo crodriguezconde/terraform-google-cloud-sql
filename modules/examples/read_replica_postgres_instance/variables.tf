6403f6e113728f1905cb6f76140becbacbe59fd7
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-21 13:55:15 +0200

    Added variables on variables.tf for main.tf file

--- modules/examples/read_replica_postgres_instance/variables.tf
+++ modules/examples/read_replica_postgres_instance/variables.tf
@@ -1,2 +1,20 @@
-variable "sql_user_password" {}
 variable "project_id" {}
+
+### Variables declaration for VPC network
+variable "vpc_network_name" {}
+variable "routing_mode" {}
+
+### Variables declaration for IP address allocation range
+variable "allocated_ip_address_range_name" {}
+variable "prefix_length" {}
+
+### Variables declaration for Cloud SQL instance
+variable "instance_name" {}
+variable "sql_region" {}
+variable "postgres_version" {}
+
+
+## Variables declaration for Cloud SQL user
+variable "sql_user_name" {}
+variable "sql_user_password" {}
+
