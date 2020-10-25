07fa70a3e7919d4bae39bcc70fe9976bd910b726
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-25 15:07:34 +0100

    Added outputs.tf variable description

--- examples/public_ip_mysql_instance/outputs.tf
+++ examples/public_ip_mysql_instance/outputs.tf
@@ -1,15 +1,19 @@
 output "self_link" {
+	description = "A self link of the newly created Cloud SQL instance."
   value = module.public_mysql_instance.self_link
 }
 
 output "connection_name" {
+  description = "The connection name for the Cloud SQL instance created. It has the following format: `[PROJECT_ID]:[REGION]:[INSTANCE_NAME]` "
   value = module.public_mysql_instance.connection_name
 }
 
 output "public_ip_address" {
+	description = "The public IPv4 address assigned to the Cloud SQL instance created."
   value = module.public_mysql_instance.public_ip_address
 }
 
 output "name" {
+	description = "Name of the Cloud SQL instance. "
   value = module.public_mysql_instance.name
 }
