7c8dcd050ff264013643b39a67c2fe946ad9f390
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-25 15:00:47 +0100

    Formatted files

--- examples/private_ip_mysql_instance/outputs.tf
+++ examples/private_ip_mysql_instance/outputs.tf
@@ -1,19 +1,19 @@
 output "self_link" {
-	description = "A self link of the newly created Cloud SQL instance."
-	value = module.private_mysql_instance.self_link
+  description = "A self link of the newly created Cloud SQL instance."
+  value       = module.private_mysql_instance.self_link
 }
 
 output "connection_name" {
-	description = "The connection name for the Cloud SQL instance created. It has the following format: `[PROJECT_ID]:[REGION]:[INSTANCE_NAME]` "
-	value = module.private_mysql_instance.connection_name
+  description = "The connection name for the Cloud SQL instance created. It has the following format: `[PROJECT_ID]:[REGION]:[INSTANCE_NAME]` "
+  value       = module.private_mysql_instance.connection_name
 }
 
 output "private_ip_address" {
-	description = "The private IPv4 address assigned to the Cloud SQL instance created."
-	value = module.private_mysql_instance.private_ip_address
+  description = "The private IPv4 address assigned to the Cloud SQL instance created."
+  value       = module.private_mysql_instance.private_ip_address
 }
 
 output "name" {
-	description = "Name of the Cloud SQL instance. "
-	value = module.private_mysql_instance.name
-} 
+  description = "Name of the Cloud SQL instance. "
+  value       = module.private_mysql_instance.name
+}
