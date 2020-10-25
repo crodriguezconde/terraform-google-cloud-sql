ac00c0aa5e5028adc8d3ce60de087a3d8447260f
Christopher Rodriguez Conde <rodriguezconde@ChristophersMBP.home>
2020-10-25 15:04:47 +0100

    Formatted files & added variables to outputs.tf

--- examples/private_ip_postres_instance/outputs.tf
+++ examples/private_ip_postres_instance/outputs.tf
@@ -1,15 +1,19 @@
 output "self_link" {
-	value = module.private_postgres_instance.self_link
+  description = "A self link of the newly created Cloud SQL instance."
+  value       = module.private_postgres_instance.self_link
 }
 
 output "connection_name" {
-	value = module.private_postgres_instance.connection_name
+  description = "The connection name for the Cloud SQL instance created. It has the following format: `[PROJECT_ID]:[REGION]:[INSTANCE_NAME]` "
+  value       = module.private_postgres_instance.connection_name
 }
 
 output "private_ip_address" {
-	value = module.private_postgres_instance.private_ip_address
+  description = "The private IPv4 address assigned to the Cloud SQL instance created."
+  value       = module.private_postgres_instance.private_ip_address
 }
 
 output "name" {
-	value = module.private_postgres_instance.name
-} 
+  description = "Name of the Cloud SQL instance. "
+  value       = module.private_postgres_instance.name
+}
--- examples/private_ip_postres_instance/terraform.tfvars
+++ examples/private_ip_postres_instance/terraform.tfvars
@@ -1,15 +1,15 @@
 ### Variables definition for VPC network
- vpc_network_name = "private-csql-vpc-for-postgres" 
- routing_mode = "GLOBAL"
+vpc_network_name = "private-csql-vpc-for-postgres"
+routing_mode     = "GLOBAL"
 
 ### Variables definition for IP address allocation range
- allocated_ip_address_range_name = "private-ip-allocation-for-postgres" 
- prefix_length = 16
+allocated_ip_address_range_name = "private-ip-allocation-for-postgres"
+prefix_length                   = 16
 
 ### Variables definition for Cloud SQL instance
- instance_name = "private-postgres-instance"
- sql_region = "europe-west1"
- postgres_version = "MYSQL_5_7"
+instance_name    = "private-postgres-instance"
+sql_region       = "europe-west1"
+postgres_version = "MYSQL_5_7"
 
 ## Variables definition for Cloud SQL user
- sql_user_name = "cloud-sql-postgres-user"
+sql_user_name = "cloud-sql-postgres-user"
